a6ad2a695014aded7fc32aff7a3dc2f403925208
YoheiIguchi <matthew.at.muse@gmail.com>
2021-02-18 14:19:35 +0900

    lint and index

--- db/sql/01_isubata.sql
+++ db/sql/01_isubata.sql
@@ -7,32 +7,35 @@ CREATE TABLE user (
   display_name TEXT,
   avatar_icon TEXT,
   created_at DATETIME NOT NULL
-) Engine=InnoDB DEFAULT CHARSET=utf8mb4;
+) Engine = InnoDB DEFAULT CHARSET = utf8mb4;
 
 DROP TABLE IF EXISTS image;
 CREATE TABLE image (
   id BIGINT UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,
   name VARCHAR(191),
   data LONGBLOB
-) Engine=InnoDB DEFAULT CHARSET=utf8mb4;
+) Engine = InnoDB DEFAULT CHARSET = utf8mb4;
 
 DROP TABLE IF EXISTS channel;
+
 CREATE TABLE channel (
   id BIGINT AUTO_INCREMENT NOT NULL PRIMARY KEY,
   name TEXT NOT NULL,
   description MEDIUMTEXT,
   updated_at DATETIME NOT NULL,
   created_at DATETIME NOT NULL
-) Engine=InnoDB DEFAULT CHARSET=utf8mb4;
+) Engine = InnoDB DEFAULT CHARSET = utf8mb4;
 
 DROP TABLE IF EXISTS message;
+
 CREATE TABLE message (
   id BIGINT AUTO_INCREMENT NOT NULL PRIMARY KEY,
   channel_id BIGINT,
   user_id BIGINT,
   content TEXT,
   created_at DATETIME NOT NULL
-) Engine=InnoDB DEFAULT CHARSET=utf8mb4;
+) Engine = InnoDB DEFAULT CHARSET = utf8mb4;
+CREATE INDEX message_channel_id on message(channel_id);
 
 DROP TABLE IF EXISTS haveread;
 CREATE TABLE haveread (
@@ -42,4 +45,4 @@ CREATE TABLE haveread (
   updated_at DATETIME NOT NULL,
   created_at DATETIME NOT NULL,
   PRIMARY KEY(user_id, channel_id)
-) Engine=InnoDB DEFAULT CHARSET=utf8mb4;
+) Engine = InnoDB DEFAULT CHARSET = utf8mb4;
