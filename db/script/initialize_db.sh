4c23230902d2b7a7f76dec9e5eee8dddc996f0d7
YoheiIguchi <matthew.at.muse@gmail.com>
2021-02-22 17:00:01 +0900

    fix

--- db/script/initialize_db.sh
+++ db/script/initialize_db.sh
@@ -4,7 +4,7 @@ set -o pipefail
 
 CURRENT_DIR=$(cd $(dirname $0);pwd)
 export MYSQL_HOST=${MYSQL_HOST:-127.0.0.1}
-export MYSQL_PORT=${MYSQL_PORT:-3306}
+export MYSQL_PORT=${MYSQL_PORT:-63306}
 export MYSQL_USER=${MYSQL_USER:-isucon}
 export MYSQL_DBNAME=${MYSQL_DBNAME:-isubata}
 export MYSQL_PWD=${MYSQL_PASS:-isucon}
@@ -17,7 +17,7 @@ do
   cat $file | mysql --defaults-file=/dev/null -h $MYSQL_HOST -P $MYSQL_PORT -u $MYSQL_USER $MYSQL_DBNAME
 done
 
-ls -1 ./*.sql.gz | while read file
+ls -1 ../sql/*.sql.gz | while read file
 do
   echo $file
   zcat $file | mysql --defaults-file=/dev/null -h $MYSQL_HOST -P $MYSQL_PORT -u $MYSQL_USER $MYSQL_DBNAME
